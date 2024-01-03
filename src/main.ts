af81107e3057fa9e3dfa62959f0c34019ff23d55
Alpharn <arkrond@gmail.com>
2024-01-03 17:55:08 +0200

    feat: enable CORS for all sources

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   const reflector = app.get(Reflector);
